a8ef4758a01369e9e10c8d23b17c4281a0eb88fa
shannjenn <shannjenn@163.com>
2018-12-29 16:10:05 +0800

    修改 Http框架优化

---
+++ app/src/main/java/com/jen/easytest/request/OrderConditionSaveRequest.java
@@ -0,0 +1,52 @@
+package com.jen.easytest.request;
+
+import com.jen.easy.EasyHttpPost;
+import com.jen.easy.EasyRequest;
+import com.jen.easy.http.HttpBasicRequest;
+import com.jen.easy.http.HttpRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 条件下单 新增条件
+ * 请求
+ *
+ * @author Created by zs on 2018/10/22.
+ */
+@EasyHttpPost(UrlAppend = "http://www.baald", Response = OrderConditionSaveResponse.class)
+public class OrderConditionSaveRequest extends HttpBasicRequest {
+    private String requestSrc = "Android";
+    private String sessionId;
+    protected String id;
+    protected String version;
+    protected String sign;
+
+    public void setSign() {
+//        sign = super.getSign();
+    }
+
+    @EasyRequest("params")
+    public List<OrderRuleModel> params;
+
+    public OrderConditionSaveRequest() {
+//        id = XGUtils.getRequestId(XGApplication.getApplication());
+//        requestSrc = "Android";
+//        version = Constant.API_VERSION;
+
+        params = new ArrayList<>();
+
+//        String userCode = XGApplication.getApplication().getMyInfo().getUserCode();
+//        try {
+//            long code = Long.parseLong(userCode);
+//            orderRuleModel.setUserId(code);
+//        } catch (NumberFormatException e) {
+//            EasyLog.e("NumberFormatException userCode：" + userCode);
+//            e.printStackTrace();
+//        }
+
+//        orderRuleModel.setUserId(XGApplication.getApplication().getMyInfo().getUserId());
+    }
+
+
+}
